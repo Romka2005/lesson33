6c18752770316f0a4c38dd44ab800d8cf10a9ddf
Romka2005 <svetanok12@gmail.com>
2022-01-22 13:10:47 +0200

    резюме

---
+++ script.js
@@ -0,0 +1,146 @@
+//Beginner_1
+// let a = Number(prompt("Введите сторону квадрата"));
+// let b = 4*a;
+// alert(b);
+// console.log(b);
+
+//Beginner_2
+// let a = +prompt("Введите сторону квадрата");
+// let b = a**2;
+// alert("S = " + b);
+// console.log("S = " + b);
+
+//Beginner_3
+// let a = +prompt("Введи 1 сторону прямоугольника");
+// let b = +prompt("Введи 2 сторону прямоугольника");
+// let c = a*b;
+// let d = 2*(a+b);
+// alert(`S = ${c}; P = ${d}`);
+// console.log(`S = ${c}; P = ${d}`);
+
+//Beginner_4
+// let a = +prompt("Введите диаметр окружности");
+// let p = 3.14;
+// let l = p*a;
+// alert(`l = ${l}`);
+// console.log(`l = ${l}`);
+
+//Beginner_5
+// let a = +prompt("Введите длину ребра куба");
+// let v = a**3;
+// let s = 6*a**2;
+// alert(`V = ${v}; S = ${s}`);
+// console.log(`V = ${v}; S = ${s}`);
+
+//Beginner_6
+// let a = +prompt("Введите 1 длину ребра прямоугольного параллелепипеда");
+// let b = +prompt("Введите 2 длину ребра прямоугольного параллелепипеда");
+// let c = +prompt("Введите 3 длину ребра прямоугольного параллелепипеда");
+// let v = a*b*c;
+// let s = 2*(a*b + b*c + a*c);
+// alert(`V = ${v}; S = ${s}`);
+// console.log(`V = ${v}; S = ${s}`);
+
+//Beginner_7
+// let r = prompt("Введите радиус");
+// let p = 3.14;
+// let l = 2*p*r;
+// let s = p*r**2;
+// alert(`L = ${l}; S = ${s}`);
+// console.log(`L = ${l}; S = ${s}`);
+
+
+//Beginner_8
+// let a = +prompt("Введите 1 число");
+// let b = +prompt("Введите 2 число");
+// let ar = (a+b)/2;
+// alert(`Среднее арифмитическое: ${ar}`);
+// console.log(`Среднее арифмитическое: ${ar}`);
+
+//Beginner_9
+// let a = +prompt("Введите 1 неотрицательное число");
+// let b = +prompt("Введите 2 неотрицательное число");
+// let gr = (a+b)**(1/2);
+// alert(`Среднее геометрическое: ${gr}`)
+// console.log(`Среднее геометрическое: ${gr}`)
+
+//Beginner_10
+// let a = +prompt("Введите 1 ненулевое число");
+// let b = +prompt("Введите 2 ненулевое число");
+// alert(`Сумма: ${(a+b)**2}; Разность: ${(a-b)**2}; Произведение: ${(a*b)**2}; Частное: ${(a/b)**2};`);
+// console.log(`Сумма: ${(a+b)**2}; Разность: ${(a-b)**2}; Произведение: ${(a*b)**2}; Частное: ${(a/b)**2};`);
+
+
+//Beginner_11
+// let a = +prompt("Введите 1 ненулевое число");
+// a = (a**2)**(1/2);
+// let b = +prompt("Введите 2 ненулевое число");
+// b = (b**2)**(1/2);
+// alert(`Сумма: ${a+b}; Разность: ${a-b}; Произведение: ${a*b}; Частное: ${a/b}`);
+// console.log(`Сумма: ${a+b}; Разность: ${a-b}; Произведение: ${a*b}; Частное: ${a/b}`);
+
+//Beginner_12
+// let a = +prompt("Введите 1 катет");
+// let b = +prompt("Введите 2 катет");
+// let c = (a**2+b**2)**1/2;
+// let p = a + b + c;
+// alert(`c = ${c}; P = ${p}`);
+// console.log(`c = ${c}; P = ${p}`);
+
+//Beginner_13
+// let p = 3.14;
+// let r1 = +prompt("Введите 1 радиус который меньше 2 радиуса");
+// let r2 = +prompt("Введите 2 радиус который больше 1 радиуса");
+// let s1 = p*((r1)**2);
+// let s2 = p*((r2)**2);
+// let s3 = s1 - s2;
+// alert(`S1 = ${s1}; S2 = ${s2}; S3 = ${s3}`);
+// console.log((`S1 = ${s1}; S2 = ${s2}; S3 = ${s3}`)
+
+//Beginner_14
+// let l = prompt("Введите длину окружности");
+// let p = 3.14;
+// let r = l/2*p;
+// let s = p * r**2;
+// alert(`R = ${r}; S = ${s}`);
+// console.log(`R = ${r}; S = ${s}`);
+
+//Beginner_15
+// let s = prompt("Введите площадь S круга");
+// let p = 3.14;
+// let d = (s*4/p)**(1/2);
+// let l = p*d;
+// alert(`L = ${l}; D = ${d}`);
+// console.log(`L = ${l}; D = ${d}`);
+
+
+
+//Beginner_25
+// let x = +prompt("Введите значение x");
+// let y = 3*(x**6)-6*(x**2)-7;
+// alert(y);
+// console.log(y);
+
+//Beginner_26
+// let x = +prompt("Введите значение x");
+// let y = 4*(x-3)**(6)-7*(x-3)**(3)+2;
+// alert(y);
+// console.log(y);
+
+//Beginner_27
+// let A = prompt("Введите число А");
+// let A2 = A**2;
+// let A4 = A**4;
+// let A8 = A**8;
+// alert(`A2 = ${A2}; A4 = ${A4} A8 = ${A8}`)
+// console.log(`A2 = ${A2}; A4 = ${A4} A8 = ${A8}`)
+
+//Beginner_28
+// let A = prompt("Введите число А");
+// let A2 = A**2;
+// let A3 = A**3;
+// let A5 = A**5;
+// let A10 = A**10;
+// let A15 = A**15;
+// alert(`A2 = ${A2};  A4 = ${A3}; A5 = ${A5}; A10 = ${A10}; A15 = ${A15}`)
+// console.log(`A2 = ${A2};  A4 = ${A3}; A5 = ${A5}; A10 = ${A10}; A15 = ${A15}`)
